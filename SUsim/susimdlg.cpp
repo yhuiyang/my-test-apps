9731d79f60b4fd147a84a375b98492a87e7f5084
YH Yang <yhuiyang@gmail.com>
2008-11-19 13:31:11 +0000

    update ui. upgrade DialogBlocks to 4.2.8. (project file update) add variable.

--- SUsim/susimdlg.cpp
+++ SUsim/susimdlg.cpp
@@ -104,6 +104,8 @@ void SUSimDlg::Init()
 {
 ////@begin SUSimDlg member initialisation
     m_boardNameTextCtrl = NULL;
+    m_response = NULL;
+    m_interface = NULL;
     m_enableBtn = NULL;
 ////@end SUSimDlg member initialisation
 }
@@ -132,54 +134,70 @@ void SUSimDlg::CreateControls()
     itemStaticBoxSizer4->Add(itemBoxSizer5, 0, wxGROW|wxALL, 5);
 
     wxStaticText* itemStaticText6 = new wxStaticText( itemDialog1, wxID_STATIC, _("Assign response board name"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemBoxSizer5->Add(itemStaticText6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 0);
+    itemBoxSizer5->Add(itemStaticText6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 10);
 
     m_boardNameTextCtrl = new wxTextCtrl( itemDialog1, ID_TEXTCTRL1, _T(""), wxDefaultPosition, wxDefaultSize, 0 );
     m_boardNameTextCtrl->SetMaxLength(15);
     m_boardNameTextCtrl->SetHelpText(_("You can assign a board name for this simulated target.\nWhen query request coming from remote, this simulated\ntarget will use the specific board name as its response\nmessage. The max length of board name is 15 bytes."));
     if (SUSimDlg::ShowToolTips())
         m_boardNameTextCtrl->SetToolTip(_("You can assign a board name for this simulated target.\nWhen query request coming from remote, this simulated\ntarget will use the specific board name as its response\nmessage. The max length of board name is 15 bytes."));
-    itemBoxSizer5->Add(m_boardNameTextCtrl, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+    itemBoxSizer5->Add(m_boardNameTextCtrl, 1, wxALIGN_CENTER_VERTICAL|wxALL, 10);
 
-    wxStaticText* itemStaticText8 = new wxStaticText( itemDialog1, wxID_STATIC, _("Select one response action"), wxDefaultPosition, wxDefaultSize, 0 );
-    itemStaticBoxSizer4->Add(itemStaticText8, 0, wxALIGN_LEFT|wxALL, 5);
+    wxBoxSizer* itemBoxSizer8 = new wxBoxSizer(wxHORIZONTAL);
+    itemStaticBoxSizer4->Add(itemBoxSizer8, 0, wxGROW|wxALL, 5);
 
-    wxArrayString itemListBox9Strings;
-    itemListBox9Strings.Add(_("No Error"));
-    itemListBox9Strings.Add(_("Syntax Error"));
-    itemListBox9Strings.Add(_("Ram Checksum Error"));
-    itemListBox9Strings.Add(_("Brec Checksum Error"));
-    itemListBox9Strings.Add(_("Unknown Load Mode"));
-    itemListBox9Strings.Add(_("Version Error"));
-    itemListBox9Strings.Add(_("Memory Allocation Error"));
-    itemListBox9Strings.Add(_("Erase Flash Error"));
-    itemListBox9Strings.Add(_("Read Flash Error"));
-    itemListBox9Strings.Add(_("Write Flash Error"));
-    itemListBox9Strings.Add(_("Firmware Size Error"));
-    itemListBox9Strings.Add(_("Bootloader Size Error"));
-    wxListBox* itemListBox9 = new wxListBox( itemDialog1, ID_LISTBOX, wxDefaultPosition, wxDefaultSize, itemListBox9Strings, wxLB_SINGLE );
-    itemListBox9->SetStringSelection(_("No Error"));
-    itemListBox9->SetHelpText(_("You can choose one result from this list box. The result\nwill be used as requested action response back to remote.\nYou can test the how the official Updater react."));
+    wxBoxSizer* itemBoxSizer9 = new wxBoxSizer(wxVERTICAL);
+    itemBoxSizer8->Add(itemBoxSizer9, 1, wxGROW|wxALL, 5);
+
+    wxStaticText* itemStaticText10 = new wxStaticText( itemDialog1, wxID_STATIC, _("Select one response action"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer9->Add(itemStaticText10, 0, wxALIGN_LEFT|wxALL, 5);
+
+    wxArrayString m_responseStrings;
+    m_responseStrings.Add(_("No Error"));
+    m_responseStrings.Add(_("Syntax Error"));
+    m_responseStrings.Add(_("Ram Checksum Error"));
+    m_responseStrings.Add(_("Brec Checksum Error"));
+    m_responseStrings.Add(_("Unknown Load Mode"));
+    m_responseStrings.Add(_("Version Error"));
+    m_responseStrings.Add(_("Memory Allocation Error"));
+    m_responseStrings.Add(_("Erase Flash Error"));
+    m_responseStrings.Add(_("Read Flash Error"));
+    m_responseStrings.Add(_("Write Flash Error"));
+    m_responseStrings.Add(_("Firmware Size Error"));
+    m_responseStrings.Add(_("Bootloader Size Error"));
+    m_response = new wxListBox( itemDialog1, ID_LISTBOX, wxDefaultPosition, wxDefaultSize, m_responseStrings, wxLB_SINGLE );
+    m_response->SetStringSelection(_("No Error"));
+    m_response->SetHelpText(_("You can choose one result from this list box. The result\nwill be used as requested action response back to remote.\nYou can test the how the official Updater react."));
     if (SUSimDlg::ShowToolTips())
-        itemListBox9->SetToolTip(_("You can choose one result from this list box. The result\nwill be used as requested action response back to remote.\nYou can test the how the official Updater react."));
-    itemStaticBoxSizer4->Add(itemListBox9, 0, wxGROW|wxALL, 5);
+        m_response->SetToolTip(_("You can choose one result from this list box. The result\nwill be used as requested action response back to remote.\nYou can test the how the official Updater react."));
+    itemBoxSizer9->Add(m_response, 1, wxGROW|wxALL, 5);
 
-    wxBoxSizer* itemBoxSizer10 = new wxBoxSizer(wxHORIZONTAL);
-    itemBoxSizer3->Add(itemBoxSizer10, 0, wxGROW|wxALL, 5);
+    wxBoxSizer* itemBoxSizer12 = new wxBoxSizer(wxVERTICAL);
+    itemBoxSizer8->Add(itemBoxSizer12, 1, wxGROW|wxALL, 5);
+
+    wxStaticText* itemStaticText13 = new wxStaticText( itemDialog1, wxID_STATIC, _("Select one interface"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer12->Add(itemStaticText13, 0, wxALIGN_LEFT|wxALL, 5);
+
+    wxArrayString m_interfaceStrings;
+    m_interface = new wxListBox( itemDialog1, ID_LISTBOX1, wxDefaultPosition, wxDefaultSize, m_interfaceStrings, wxLB_SINGLE );
+    itemBoxSizer12->Add(m_interface, 1, wxGROW|wxALL, 5);
+
+    wxBoxSizer* itemBoxSizer15 = new wxBoxSizer(wxHORIZONTAL);
+    itemBoxSizer3->Add(itemBoxSizer15, 0, wxGROW|wxALL, 5);
 
     m_enableBtn = new wxToggleButton( itemDialog1, ID_TOGGLEBUTTON, _("Enable"), wxDefaultPosition, wxDefaultSize, 0 );
     m_enableBtn->SetValue(false);
     m_enableBtn->SetHelpText(_("A toggle button to enable/disable the simulated\ntarget functions."));
     if (SUSimDlg::ShowToolTips())
         m_enableBtn->SetToolTip(_("A toggle button to enable/disable the simulated\ntarget functions."));
-    itemBoxSizer10->Add(m_enableBtn, 0, wxGROW|wxALL, 5);
+    itemBoxSizer15->Add(m_enableBtn, 0, wxGROW|wxALL, 5);
 
-    wxStaticBox* itemStaticBoxSizer12Static = new wxStaticBox(itemDialog1, wxID_ANY, _("Transaction Log"));
-    wxStaticBoxSizer* itemStaticBoxSizer12 = new wxStaticBoxSizer(itemStaticBoxSizer12Static, wxVERTICAL);
-    itemBoxSizer2->Add(itemStaticBoxSizer12, 1, wxGROW|wxALL, 5);
+    wxStaticBox* itemStaticBoxSizer17Static = new wxStaticBox(itemDialog1, wxID_ANY, _("Transaction Log"));
+    wxStaticBoxSizer* itemStaticBoxSizer17 = new wxStaticBoxSizer(itemStaticBoxSizer17Static, wxVERTICAL);
+    itemBoxSizer2->Add(itemStaticBoxSizer17, 1, wxGROW|wxALL, 5);
 
-    wxTextCtrl* itemTextCtrl13 = new wxTextCtrl( itemDialog1, ID_TEXTCTRL, _T(""), wxDefaultPosition, wxSize(800, 300), wxTE_MULTILINE );
-    itemStaticBoxSizer12->Add(itemTextCtrl13, 1, wxGROW|wxALL, 5);
+    wxTextCtrl* itemTextCtrl18 = new wxTextCtrl( itemDialog1, ID_TEXTCTRL, _T(""), wxDefaultPosition, wxSize(800, 300), wxTE_MULTILINE );
+    itemStaticBoxSizer17->Add(itemTextCtrl18, 1, wxGROW|wxALL, 5);
 
 ////@end SUSimDlg content construction
 }
@@ -219,3 +237,8 @@ wxIcon SUSimDlg::GetIconResource( const wxString& name )
     return wxNullIcon;
 ////@end SUSimDlg icon retrieval
 }
+
+
+
+
+

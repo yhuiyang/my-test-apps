e90f38a28c3d8d2c0a650249bc3c7a099b62d1c9
YH Yang <yhuiyang@gmail.com>
2008-12-17 08:59:43 +0000

    Add frame to mimic the Total Phase Aardvark Adapter Control Center app.

---
+++ AardvarkApp/build/tpaaframe.cpp
@@ -0,0 +1,209 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        tpaaframe.cpp
+// Purpose:     
+// Author:      yhuiyang@gmail.com
+// Modified by: 
+// Created:     12/17/2008 2:16:56 PM
+// RCS-ID:      
+// Copyright:   
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+////@begin includes
+////@end includes
+
+#include "tpaaframe.h"
+
+////@begin XPM images
+////@end XPM images
+
+
+/*!
+ * TPAAFrame type definition
+ */
+
+IMPLEMENT_CLASS( TPAAFrame, wxFrame )
+
+
+/*!
+ * TPAAFrame event table definition
+ */
+
+BEGIN_EVENT_TABLE( TPAAFrame, wxFrame )
+
+////@begin TPAAFrame event table entries
+////@end TPAAFrame event table entries
+
+END_EVENT_TABLE()
+
+
+/*!
+ * TPAAFrame constructors
+ */
+
+TPAAFrame::TPAAFrame()
+{
+    Init();
+}
+
+TPAAFrame::TPAAFrame( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+    Init();
+    Create( parent, id, caption, pos, size, style );
+}
+
+
+/*!
+ * TPAAFrame creator
+ */
+
+bool TPAAFrame::Create( wxWindow* parent, wxWindowID id, const wxString& caption, const wxPoint& pos, const wxSize& size, long style )
+{
+////@begin TPAAFrame creation
+    wxFrame::Create( parent, id, caption, pos, size, style );
+
+    CreateControls();
+    SetIcon(GetIconResource(wxT("../image/Aardvark GUI.ico")));
+    Centre();
+////@end TPAAFrame creation
+    return true;
+}
+
+
+/*!
+ * TPAAFrame destructor
+ */
+
+TPAAFrame::~TPAAFrame()
+{
+////@begin TPAAFrame destruction
+////@end TPAAFrame destruction
+}
+
+
+/*!
+ * Member initialisation
+ */
+
+void TPAAFrame::Init()
+{
+////@begin TPAAFrame member initialisation
+////@end TPAAFrame member initialisation
+}
+
+
+/*!
+ * Control creation for TPAAFrame
+ */
+
+void TPAAFrame::CreateControls()
+{    
+////@begin TPAAFrame content construction
+    TPAAFrame* itemFrame1 = this;
+
+    wxMenuBar* menuBar = new wxMenuBar;
+    wxMenu* itemMenu3 = new wxMenu;
+    itemMenu3->Append(ID_MENUITEM_FILE_CLOSE, _("Close"), _T(""), wxITEM_NORMAL);
+    menuBar->Append(itemMenu3, _("&File"));
+    wxMenu* itemMenu5 = new wxMenu;
+    itemMenu5->Append(ID_MENUITEM_HELP_ABOUT, _("&About"), _T(""), wxITEM_NORMAL);
+    menuBar->Append(itemMenu5, _("&Help"));
+    itemFrame1->SetMenuBar(menuBar);
+
+    wxStatusBar* itemStatusBar7 = new wxStatusBar( itemFrame1, ID_STATUSBAR, wxST_SIZEGRIP|wxNO_BORDER );
+    itemStatusBar7->SetFieldsCount(1);
+    itemFrame1->SetStatusBar(itemStatusBar7);
+
+    wxBoxSizer* itemBoxSizer8 = new wxBoxSizer(wxVERTICAL);
+    itemFrame1->SetSizer(itemBoxSizer8);
+
+    wxSplitterWindow* itemSplitterWindow9 = new wxSplitterWindow( itemFrame1, ID_SPLITTERWINDOW, wxDefaultPosition, wxDefaultSize, wxSP_BORDER|wxSP_3DSASH|wxNO_BORDER );
+    itemSplitterWindow9->SetMinimumPaneSize(200);
+
+    wxPanel* itemPanel10 = new wxPanel( itemSplitterWindow9, ID_PANEL_CONFIG, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER|wxTAB_TRAVERSAL );
+    wxBoxSizer* itemBoxSizer11 = new wxBoxSizer(wxHORIZONTAL);
+    itemPanel10->SetSizer(itemBoxSizer11);
+
+    itemBoxSizer11->Add(5, 5, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+    wxButton* itemButton13 = new wxButton( itemPanel10, ID_BUTTON_CONFIG_AA_DLG, _("Configure Aardvark Adapter"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemButton13->SetFont(wxFont(18, wxSWISS, wxNORMAL, wxBOLD, false, wxT("Courier New")));
+    itemBoxSizer11->Add(itemButton13, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+    itemBoxSizer11->Add(5, 5, 1, wxALIGN_CENTER_VERTICAL|wxALL, 5);
+
+    wxPanel* itemPanel15 = new wxPanel( itemSplitterWindow9, ID_PANEL_LOG, wxDefaultPosition, wxDefaultSize, wxSUNKEN_BORDER|wxTAB_TRAVERSAL );
+    wxBoxSizer* itemBoxSizer16 = new wxBoxSizer(wxVERTICAL);
+    itemPanel15->SetSizer(itemBoxSizer16);
+
+    wxStaticText* itemStaticText17 = new wxStaticText( itemPanel15, wxID_STATIC, _("Transaction Log:"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer16->Add(itemStaticText17, 0, wxALIGN_LEFT|wxALL, 5);
+
+    wxTextCtrl* itemTextCtrl18 = new wxTextCtrl( itemPanel15, ID_LOGWIN, _T(""), wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH2|wxDOUBLE_BORDER );
+    itemBoxSizer16->Add(itemTextCtrl18, 1, wxGROW|wxLEFT|wxRIGHT|wxTOP, 5);
+
+    wxBoxSizer* itemBoxSizer19 = new wxBoxSizer(wxHORIZONTAL);
+    itemBoxSizer16->Add(itemBoxSizer19, 0, wxALIGN_RIGHT|wxLEFT|wxTOP|wxBOTTOM, 5);
+    wxButton* itemButton20 = new wxButton( itemPanel15, ID_BUTTON_LOG_CLEAR, _("Clear Log"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer19->Add(itemButton20, 0, wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT, 5);
+
+    wxButton* itemButton21 = new wxButton( itemPanel15, ID_BUTTON_LOG_SAVE, _("Save to File"), wxDefaultPosition, wxDefaultSize, 0 );
+    itemBoxSizer19->Add(itemButton21, 0, wxALIGN_CENTER_VERTICAL|wxLEFT|wxRIGHT, 5);
+
+    itemSplitterWindow9->SplitHorizontally(itemPanel10, itemPanel15, -200);
+    itemBoxSizer8->Add(itemSplitterWindow9, 1, wxGROW, 0);
+
+////@end TPAAFrame content construction
+}
+
+
+/*!
+ * Should we show tooltips?
+ */
+
+bool TPAAFrame::ShowToolTips()
+{
+    return true;
+}
+
+/*!
+ * Get bitmap resources
+ */
+
+wxBitmap TPAAFrame::GetBitmapResource( const wxString& name )
+{
+    // Bitmap retrieval
+////@begin TPAAFrame bitmap retrieval
+    wxUnusedVar(name);
+    return wxNullBitmap;
+////@end TPAAFrame bitmap retrieval
+}
+
+/*!
+ * Get icon resources
+ */
+
+wxIcon TPAAFrame::GetIconResource( const wxString& name )
+{
+    // Icon retrieval
+////@begin TPAAFrame icon retrieval
+    wxUnusedVar(name);
+    if (name == _T("../image/Aardvark GUI.ico"))
+    {
+        wxIcon icon(_T("../image/Aardvark GUI.ico"), wxBITMAP_TYPE_ICO);
+        return icon;
+    }
+    return wxNullIcon;
+////@end TPAAFrame icon retrieval
+}
---
+++ AardvarkApp/build/tpaaframe.h
@@ -0,0 +1,106 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        tpaaframe.h
+// Purpose:     
+// Author:      yhuiyang@gmail.com
+// Modified by: 
+// Created:     12/17/2008 2:16:56 PM
+// RCS-ID:      
+// Copyright:   
+// Licence:     
+/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _TPAAFRAME_H_
+#define _TPAAFRAME_H_
+
+
+/*!
+ * Includes
+ */
+
+////@begin includes
+#include "wx/frame.h"
+#include "wx/statusbr.h"
+#include "wx/splitter.h"
+////@end includes
+
+/*!
+ * Forward declarations
+ */
+
+////@begin forward declarations
+////@end forward declarations
+
+/*!
+ * Control identifiers
+ */
+
+////@begin control identifiers
+#define SYMBOL_TPAAFRAME_STYLE wxDEFAULT_FRAME_STYLE|wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxCLOSE_BOX
+#define SYMBOL_TPAAFRAME_TITLE _("Total Phase Aardvark I2C/SPI Control Center fork")
+#define SYMBOL_TPAAFRAME_IDNAME ID_TPAAFRAME
+#define SYMBOL_TPAAFRAME_SIZE wxSize(860, 640)
+#define SYMBOL_TPAAFRAME_POSITION wxDefaultPosition
+////@end control identifiers
+
+
+/*!
+ * TPAAFrame class declaration
+ */
+
+class TPAAFrame: public wxFrame
+{    
+    DECLARE_CLASS( TPAAFrame )
+    DECLARE_EVENT_TABLE()
+
+public:
+    /// Constructors
+    TPAAFrame();
+    TPAAFrame( wxWindow* parent, wxWindowID id = SYMBOL_TPAAFRAME_IDNAME, const wxString& caption = SYMBOL_TPAAFRAME_TITLE, const wxPoint& pos = SYMBOL_TPAAFRAME_POSITION, const wxSize& size = SYMBOL_TPAAFRAME_SIZE, long style = SYMBOL_TPAAFRAME_STYLE );
+
+    bool Create( wxWindow* parent, wxWindowID id = SYMBOL_TPAAFRAME_IDNAME, const wxString& caption = SYMBOL_TPAAFRAME_TITLE, const wxPoint& pos = SYMBOL_TPAAFRAME_POSITION, const wxSize& size = SYMBOL_TPAAFRAME_SIZE, long style = SYMBOL_TPAAFRAME_STYLE );
+
+    /// Destructor
+    ~TPAAFrame();
+
+    /// Initialises member variables
+    void Init();
+
+    /// Creates the controls and sizers
+    void CreateControls();
+
+////@begin TPAAFrame event handler declarations
+
+////@end TPAAFrame event handler declarations
+
+////@begin TPAAFrame member function declarations
+
+    /// Retrieves bitmap resources
+    wxBitmap GetBitmapResource( const wxString& name );
+
+    /// Retrieves icon resources
+    wxIcon GetIconResource( const wxString& name );
+////@end TPAAFrame member function declarations
+
+    /// Should we show tooltips?
+    static bool ShowToolTips();
+
+////@begin TPAAFrame member variables
+    /// Control identifiers
+    enum {
+        ID_TPAAFRAME = 10004,
+        ID_MENUITEM_FILE_CLOSE = 10006,
+        ID_MENUITEM_HELP_ABOUT = 10008,
+        ID_STATUSBAR = 10007,
+        ID_SPLITTERWINDOW = 10009,
+        ID_PANEL_CONFIG = 10010,
+        ID_BUTTON_CONFIG_AA_DLG = 10012,
+        ID_PANEL_LOG = 10011,
+        ID_LOGWIN = 10013,
+        ID_BUTTON_LOG_CLEAR = 10014,
+        ID_BUTTON_LOG_SAVE = 10015
+    };
+////@end TPAAFrame member variables
+};
+
+#endif
+    // _TPAAFRAME_H_

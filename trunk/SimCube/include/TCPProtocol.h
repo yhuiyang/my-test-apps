3708e820466ce35a45bb79a7aaa3f8866896c0d3
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-08-18 12:39:24 +0000

    Add include guard.

---
+++ trunk/SimCube/include/TCPProtocol.h
@@ -0,0 +1,5 @@
+#ifndef _TCP_PROTOCOL_H_
+#define _TCP_PROTOCOL_H_
+
+#endif /* _TCP_PROTOCOL_H_ */
+
---
+++ trunk/SimCube/include/UDPProtocol.h
@@ -0,0 +1,5 @@
+#ifndef _UDP_PROTOCOL_H_
+#define _UDP_PROTOCOL_H_
+
+#endif /* _UDP_PROTOCOL_H_ */
+

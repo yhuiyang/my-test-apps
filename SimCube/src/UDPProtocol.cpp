544dcaa85dfdf637d38da1bccc5e2e72829a6269
YH Yang <yhuiyang@gmail.com>
2009-08-24 02:33:32 +0000

    Use pointer to C++ member function to dispatch event to handler by keyword.

--- SimCube/src/UDPProtocol.cpp
+++ SimCube/src/UDPProtocol.cpp
@@ -113,7 +113,50 @@ void UDPProtocol::ProcessDownloadModeProtocol(const char *buf, size_t len,
 void UDPProtocol::ProcessNormalModeProtocol(const char *buf, size_t len,
                                             wxSockAddress &addr)
 {
+    bool handled = false;
+    int msg_keyword_checker, handler, rIdx, lIdx;
+    wxString token;
     wxStringTokenizer tkz(wxString::FromAscii(buf), wxT(";"));
+    while (tkz.HasMoreTokens())
+    {
+        token = tkz.GetNextToken();
+        if (token.IsEmpty())
+            continue;
+        msg_keyword_checker = 0;
+        for (handler = 0; 
+            _normalHandler[handler].keyword != MSG_KEYWORD_INVALID;
+            handler++)
+        {
+            lIdx = token.Find(_normalHandler[handler].keyword);
+            if (lIdx != wxNOT_FOUND)
+            {
+                /* found keyword for current handler */
+                msg_keyword_checker |= 0x01 << (2 * handler);
+                rIdx = token.Find(_normalHandler[handler].keyword, true);
+                if (lIdx != rIdx)
+                {
+                    /* found multiple keywords for handler in current token */
+                    msg_keyword_checker |= 0x02 << (2 * handler);
+                }
+            }
+        }
+
+        /* skip if no keyword */
+        if (msg_keyword_checker == 0)
+            continue;
+
+        /* process token if only single keyword */
+        if (!(msg_keyword_checker & (msg_keyword_checker - 1)))
+        {
+            /* find handler type */
+            for (handler = 0; msg_keyword_checker != 0x01; msg_keyword_checker >>= 2);
+            /* process token by handler type */
+            handled = (this->*_normalHandler[handler].handler)(buf, len, addr);
+
+            if (!handled)
+                wxLogVerbose(_("Token (%s) didn't handle by handler"), token);
+        }
+    }
 }
 
 bool UDPProtocol::set_request_handler(const char *buf, size_t len,

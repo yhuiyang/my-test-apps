7452ae2fb4e07079235fe7e6bef74b2492eba30b
YH Yang <yhuiyang@gmail.com>
2009-08-31 06:33:19 +0000

    Add basic tcp server function.

--- SimCube/include/TCPProtocol.h
+++ SimCube/include/TCPProtocol.h
@@ -1,5 +1,21 @@
 #ifndef _TCP_PROTOCOL_H_
 #define _TCP_PROTOCOL_H_
 
+#include <wx/wx.h>
+#include <wx/socket.h>
+
+class TCPProtocol : public wxEvtHandler
+{
+public:
+    TCPProtocol();
+    ~TCPProtocol();
+
+private:
+    /* event handlers */
+    void OnSocketEvent(wxSocketEvent &event);
+
+    DECLARE_EVENT_TABLE()
+};
+
 #endif /* _TCP_PROTOCOL_H_ */
 

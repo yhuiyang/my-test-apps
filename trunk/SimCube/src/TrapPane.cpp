aa53efe9ec012c717cb70f70d80ccdb46cc9d3b1
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-09-01 19:13:25 +0000

    update trap layout.

--- trunk/SimCube/src/TrapPane.cpp
+++ trunk/SimCube/src/TrapPane.cpp
@@ -1,4 +1,5 @@
 #include <wx/wx.h>
+#include "SimCubeApp.h"
 #include "TrapPane.h"
 
 TrapPane::TrapPane()
@@ -29,10 +30,44 @@ bool TrapPane::Create(wxWindow *parent, wxWindowID id, const wxPoint &pos,
 
 void TrapPane::Init()
 {
+    _db = wxGetApp().GetMainDatabase();
 }
 
 void TrapPane::CreateControls()
 {
+    wxSizer *allSizer = new wxBoxSizer(wxVERTICAL);
+    wxStaticBoxSizer *ledSizer = new wxStaticBoxSizer(wxHORIZONTAL, this, _("Led Status"));
+    allSizer->Add(ledSizer, 0, wxALL | wxEXPAND, 5);
+    wxArrayString ledStatus;
+    ledStatus.push_back(_("Off"));
+    ledStatus.push_back(_("Red"));
+    ledStatus.push_back(_("Green"));
+    ledStatus.push_back(_("Orange"));
+    ledStatus.push_back(_("Green<->Off"));
+    ledStatus.push_back(_("Off<->Green"));
+    ledStatus.push_back(_("Red<->Green"));
+    ledStatus.push_back(_("Green<->Orange"));
+    wxRadioBox *powerRB = new wxRadioBox(this, wxID_ANY, _("Power"), wxDefaultPosition,
+        wxDefaultSize, ledStatus, 0, wxVERTICAL);
+    wxRadioBox *fanRB = new wxRadioBox(this, wxID_ANY, _("Fan"), wxDefaultPosition,
+        wxDefaultSize, ledStatus, 0, wxVERTICAL);
+    wxRadioBox *lampaRB = new wxRadioBox(this, wxID_ANY, _("LampA"), wxDefaultPosition,
+        wxDefaultSize, ledStatus, 0, wxVERTICAL);
+    wxRadioBox *lampbRB = new wxRadioBox(this, wxID_ANY, _("LampB"), wxDefaultPosition,
+        wxDefaultSize, ledStatus, 0, wxVERTICAL);
+    ledSizer->Add(powerRB, 0, wxALL|wxEXPAND, 5);
+    ledSizer->Add(fanRB, 0, wxALL|wxEXPAND, 5);
+    ledSizer->Add(lampaRB, 0, wxALL|wxEXPAND, 5);
+    ledSizer->Add(lampbRB, 0, wxALL|wxEXPAND, 5);
+
+    wxBoxSizer *lampSizer = new wxBoxSizer(wxHORIZONTAL);
+    allSizer->Add(lampSizer, 1, wxALL | wxEXPAND, 5);
+    wxStaticBoxSizer *lampASizer = new wxStaticBoxSizer(wxVERTICAL, this, _("Lamp A Status"));
+    lampSizer->Add(lampASizer, 1, wxALL | wxEXPAND, 0);
+    wxStaticBoxSizer *lampBSizer = new wxStaticBoxSizer(wxVERTICAL, this, _("Lamp B Status"));
+    lampSizer->Add(lampBSizer, 1, wxALL | wxEXPAND, 0);
+    
+    SetSizerAndFit(allSizer);
 }
 
 

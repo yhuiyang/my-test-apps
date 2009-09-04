261f78888a968034b556353dee4b2a784734a4f9
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-09-04 08:21:36 +0000

    update.

--- trunk/SimCube/src/SimCubeFrame.cpp
+++ trunk/SimCube/src/SimCubeFrame.cpp
@@ -52,9 +52,8 @@ void SimCubeAboutDialog::CreateControls()
     wx->Add(new wxStaticText(this, wxID_STATIC, wxVERSION_STRING), 0, wxALL, 5);
     wxSizer *sqlite = new wxBoxSizer(wxHORIZONTAL);
     lib->Add(sqlite, 0, wxALL | wxEXPAND, 0);
-    sqlite->Add(new wxStaticText(this, wxID_STATIC, wxT("SQLite")), 0, wxALL, 5);
     sqlite->Add(new wxStaticText(this, wxID_STATIC, 
-        wxGetApp().GetMainDatabase()->GetVersion()), 0, wxALL, 5);
+        wxT("SQLite ") + wxGetApp().GetMainDatabase()->GetVersion()), 0, wxALL, 5);
 
     /* button */
     wxButton *ok = new wxButton(this, wxID_ANY, _("OK"));

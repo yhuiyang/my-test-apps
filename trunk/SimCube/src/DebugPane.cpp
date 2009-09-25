869ab1824a06b0d6806d6999e12c699acb7ee810
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-09-25 11:10:10 +0000

    update layout.

--- trunk/SimCube/src/DebugPane.cpp
+++ trunk/SimCube/src/DebugPane.cpp
@@ -39,26 +39,25 @@ void DebugPane::Init()
 
 void DebugPane::CreateControls()
 {
-    wxBoxSizer *allSizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *allSizer = new wxBoxSizer(wxHORIZONTAL);
     /* log text ctrl */
     _logTextCtrl = new wxTextCtrl(this, wxID_ANY, wxEmptyString,
         wxDefaultPosition, wxDefaultSize,
         wxTE_MULTILINE|wxTE_READONLY|wxTE_RICH2);
-    allSizer->Add(_logTextCtrl, 1, wxALL|wxEXPAND, 5);
+    allSizer->Add(_logTextCtrl, 1, wxTOP|wxBOTTOM|wxLEFT|wxEXPAND, 5);
   
     /* ctrl buttons */
-    wxBoxSizer *ctrlSizer = new wxBoxSizer(wxHORIZONTAL);
-    allSizer->Add(ctrlSizer, 0, wxBOTTOM|wxEXPAND, 5);
+    wxBoxSizer *ctrlSizer = new wxBoxSizer(wxVERTICAL);
+    allSizer->Add(ctrlSizer, 0, wxALL|wxEXPAND, 0);
     wxBitmapButton *save = new wxBitmapButton(this, wxID_ANY, wxBitmap(save_16_xpm));
     save->SetToolTip(_("Save log text to file"));
     wxBitmapButton *erase = new wxBitmapButton(this, wxID_ANY, wxBitmap(delete_16_xpm));
     erase->SetToolTip(_("Erase all log text"));
     wxBitmapButton *verbose = new wxBitmapButton(this, wxID_ANY, wxBitmap(*_verboseImg));
     verbose->SetToolTip(_("Verbose"));
-    ctrlSizer->AddStretchSpacer();
-    ctrlSizer->Add(verbose, 0, wxRIGHT|wxEXPAND, 5);
-    ctrlSizer->Add(save, 0, wxALL|wxEXPAND, 0);
-    ctrlSizer->Add(erase, 0, wxRIGHT|wxEXPAND, 5);
+    ctrlSizer->Add(save, 0, wxTOP, 5);
+    ctrlSizer->Add(erase, 0, wxALL, 0);
+    ctrlSizer->Add(verbose, 0, wxTOP|wxBOTTOM, 5);
     SetSizer(allSizer);
 }
 

302ea33ab3455d88b34331ae7bee2f57ec5781a2
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-09-27 11:44:45 +0000

    Gen version info from svn working copy.

---
+++ trunk/SimCube/include/Version_template.h
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////////
+// THIS FILE IS GENERATED AUTOMATICALLY, DO NOT MODIFY IT BY HAND.
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef _SIMCUBE_VERSION_H_
+#define _SIMCUBE_VERSION_H_
+
+#define SCVER_MAJOR_STRING      "1"
+#define SCVER_MINOR_STRING      "0"
+#define SCVER_RELEASE_STRING    "0"
+#define SCVER_BUILD_STRING      "$WCREV$$WCMODS?*:$"
+
+#endif /* _VERSION_H_ */

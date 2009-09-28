2a1a4614f39c6e7b4f49015076fa21108570d0b6
YH Yang <yhuiyang@gmail.com>
2009-09-28 07:06:46 +0000

    Let windows resource also use version generated by SubWCRev.

--- SimCube/include/Version_template.h
+++ SimCube/include/Version_template.h
@@ -5,10 +5,16 @@
 #ifndef _SIMCUBE_VERSION_H_
 #define _SIMCUBE_VERSION_H_
 
-#define SCVER_MAJOR_STRING      "1"
-#define SCVER_MINOR_STRING      "0"
-#define SCVER_RELEASE_STRING    "0"
-#define SCVER_BUILD_STRING      "$WCREV$$WCMODS?*:$"
+#define SCVER_MAJOR             1
+#define SCVER_MINOR             0
+#define SCVER_RELEASE           0
+#define SCVER_BUILD             $WCREV$$WCMODS?*:$
+#define STRINGIFY_HELPER(x)     #x
+#define STRINGIFY(x)            STRINGIFY_HELPER(x)
+#define SCVER_MAJOR_STRING      STRINGIFY(SCVER_MAJOR)
+#define SCVER_MINOR_STRING      STRINGIFY(SCVER_MINOR)
+#define SCVER_RELEASE_STRING    STRINGIFY(SCVER_RELEASE)
+#define SCVER_BUILD_STRING      STRINGIFY(SCVER_BUILD)
 #define SC_BUILDDATE_STRING     "$WCNOW$";
 
 #endif /* _VERSION_H_ */

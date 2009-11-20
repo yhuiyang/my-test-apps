7be88e5865c21eff5aff8d17310c35bb559f7152
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2009-11-20 09:12:11 +0000

    Add CL200 Short/Long Message Parser.

--- trunk/SimCL200/include/SimCL200Frame.h
+++ trunk/SimCL200/include/SimCL200Frame.h
@@ -2,10 +2,13 @@
 #define _SIMCL200FRAME_H_
 
 #include <wx/wx.h>
+#include <wx/timer.h>
 #include <wx/ribbon/bar.h>
 #include <wx/ribbon/buttonbar.h>
 #include <wx/ribbon/toolbar.h>
 #include <wx/ribbon/gallery.h>
+
+class wxSerialPort;
 
 class SimCL200Frame: public wxFrame
 {    
@@ -32,10 +35,23 @@ private:
     void Init();
     void CreateControls();
 
+    void OnReadSerialPortTimer(wxTimerEvent &event);
     void OnBaudRateClicked(wxRibbonButtonBarEvent &event);
     void OnBaudRateDropDown(wxRibbonButtonBarEvent &event);
 
+    void AppendDataToRingBuffer(unsigned char *buf, size_t len);
+    void ParseDataLengthFinish(unsigned long len);
+    unsigned char GetDataAtRingBufferOffset(unsigned long offset);
+    void ProcessCL200ShortMessage(unsigned char *msg);
+    void ProcessCL200LongMessage(unsigned char *msg);
+
     wxRibbonBar *m_ribbon;
+    wxTimer *m_readTimer;
+    wxSerialPort *m_port;
+    unsigned char *m_ringBuffer;
+    unsigned long m_parseIndex;
+    unsigned long m_appendIndex;
+    wxDateTime m_previousAppendTime;
 
     DECLARE_CLASS(SimCL200Frame)
     DECLARE_EVENT_TABLE()

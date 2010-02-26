7aa208b5c3a3bae20f9d9102265f90533a3b847a
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2010-02-26 11:07:48 +0000

    Use non-blocking read method in search thread, and send target found event to main thread. (When search thread is running, the CPU usage is higher than before)

--- trunk/Updater2/src/TargetsPane.cpp
+++ trunk/Updater2/src/TargetsPane.cpp
@@ -23,6 +23,7 @@ public:
 
 private:
     TargetsPane *_pHandler;
+    unsigned char *_recvBuf;
 };
 
 typedef enum
@@ -47,6 +48,7 @@ public:
 // ------------------------------------------------------------------------
 // Implementation
 // ------------------------------------------------------------------------
+#define RECVBUFSIZE 128
 SearchThread::SearchThread(TargetsPane *handler) 
     : wxThread(wxTHREAD_DETACHED), _pHandler(handler)
 {
@@ -58,6 +60,8 @@ SearchThread::SearchThread(TargetsPane *handler)
         local.Hostname(netAdapter.at(0).GetIp());
         netAdapter.at(0).udp = new wxDatagramSocket(local, wxSOCKET_NOWAIT);
     }
+
+    _recvBuf = new unsigned char[RECVBUFSIZE];
 }
 
 SearchThread::~SearchThread()
@@ -67,6 +71,8 @@ SearchThread::~SearchThread()
     {
         wxDELETE(netAdapter.at(0).udp);
     }
+
+    wxDELETE(_recvBuf);
 }
 
 wxThread::ExitCode SearchThread::Entry()
@@ -74,31 +80,58 @@ wxThread::ExitCode SearchThread::Entry()
     int loop;
     wxThreadEvent event(wxEVT_COMMAND_THREAD, myID_SEARCH_THREAD);
     SearchThreadMessage msg;
-    unsigned char targetQueryCommand[] =
+    const unsigned char targetQueryCommand[] =
     {
         0x53, 0x51, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     };
+    const unsigned char targetReplyCommand[] =
+    {
+        0x73, 0x71, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
     wxVector<NetAdapter> &netAdapter = wxGetApp().m_Adapters;
-    wxIPV4address broadcast;
+    wxIPV4address broadcast, remote;
     broadcast.Hostname(netAdapter.at(0).GetBroadcast());
     broadcast.Service(40000);
+    wxDatagramSocket *udpSocket = netAdapter.at(0).udp;
+
+    wxLogMessage(wxT("Target search thread is running!"));
     
-    for (loop = 0; (loop < 5) && !TestDestroy(); loop++)
+    for (loop = 0; (loop < 4) && !TestDestroy(); loop++)
     {
-        wxLogMessage(wxT("Message from thread[%ld], loop = %d"), GetId(), loop);
-        netAdapter.at(0).udp->SendTo(broadcast, targetQueryCommand, 16);
+        wxLogVerbose(wxT("Broadcast target discovery command!"));
+        udpSocket->SendTo(broadcast, targetQueryCommand, 16);
 
-        if (loop == 2)
+        wxDateTime start = wxDateTime::Now(), current;
+        while (!TestDestroy())
         {
-            msg.type = SEARCH_THREAD_TARGET_FOUND;
-            msg.ip = wxT("192.168.1.1");
-            msg.name = wxT("Winner");
-            event.SetPayload(msg);
-            wxQueueEvent(_pHandler, event.Clone());
+            udpSocket->RecvFrom(remote, _recvBuf, RECVBUFSIZE);
+            if (!udpSocket->Error())
+            {
+                wxUint32 count = udpSocket->LastCount();
+                if (count > 0)
+                {
+                    wxLogVerbose(wxT("Receive %d byte(s) data from %s"),
+                        count, remote.IPAddress());
+                    if ((count > 16) && (count <= 36))
+                    {
+                        if (!memcmp(_recvBuf, targetReplyCommand, 16))
+                        {
+                            msg.type = SEARCH_THREAD_TARGET_FOUND;
+                            msg.ip = remote.IPAddress();
+                            msg.name = wxString(&_recvBuf[16]);
+                            event.SetPayload(msg);
+                            wxQueueEvent(_pHandler, event.Clone());
+                        }
+                    }
+                }                
+            }
+            current = wxDateTime::Now();
+            wxTimeSpan diff = current.Subtract(start);
+            if (diff.GetSeconds() >= 3)
+                break;
         }
-
-        wxMilliSleep(3000);
     }
 
     /* thread completed */

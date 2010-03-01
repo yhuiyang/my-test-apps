5aac2a1e5d25e08a7a2cd684179b2b68392d4016
YH Yang <yhuiyang@gmail.com>
2010-03-01 08:06:19 +0000

    Move search thread code.

---
+++ Updater2/include/SearchThread.h
@@ -0,0 +1,39 @@
+#ifndef _SEARCH_THREAD_H_
+#define _SEARCH_THREAD_H_
+
+#include <wx/wx.h>
+
+class SearchThread : public wxThread
+{
+public:
+    SearchThread(wxEvtHandler *handler);
+    ~SearchThread();
+    virtual wxThread::ExitCode Entry();
+
+private:
+    wxString LookupRemoteMAC(const wxString &ipAddress);
+    wxEvtHandler *_pHandler;
+    unsigned char *_recvBuf;
+};
+
+typedef enum
+{
+    SEARCH_THREAD_COMPLETED,
+    SEARCH_THREAD_TARGET_FOUND,
+} STMType;
+
+class SearchThreadMessage
+{
+public:
+    SearchThreadMessage()
+    {
+        type = SEARCH_THREAD_COMPLETED;
+        ip = name = mac = wxEmptyString;
+    }
+    STMType type;
+    wxString ip;
+    wxString name;
+    wxString mac;
+};
+
+#endif /* _SEARCH_THREAD_H_ */

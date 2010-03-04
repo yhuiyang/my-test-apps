d51fcfa8d16d9b62a2d2defeaf4084758f01f3dc
YH Yang <yhuiyang@gmail.com>
2010-03-04 11:58:47 +0000

    Add some update process.

--- Updater2/include/UpdateThread.h
+++ Updater2/include/UpdateThread.h
@@ -3,6 +3,7 @@
 
 #include <wx/wx.h>
 #include <wx/thread.h>
+#include <wx/socket.h>
 
 class UpdateThread : public wxThread
 {
@@ -14,7 +15,10 @@ public:
 private:
     wxEvtHandler *_pHandler;
     wxString _targetIpAddress;
+    wxString _localIpAddress;
     int _row;
+    wxSocketClient *_tcp;
+    unsigned char *_recvBuf;
 };
 
 typedef enum
@@ -37,6 +41,30 @@ public:
     int row;
     int error;
 };
+
+// -------------------------------------------------------------------------
+// Thread error code definition
+// The SOI official updater uses postive number as error code, and we follow
+// its error number, and use negative number for internal error code
+// -------------------------------------------------------------------------
+#define UTERROR_NOERROR             0
+
+/* error code compatible with SOI updater */
+#define UTERROR_BAD_IMAGE_SYNTAX    2
+#define UTERROR_RAM_CHECKSUM        3
+#define UTERROR_BREC_CHECKSUM       4
+#define UTERROR_LOAD_MODE           5
+#define UTERROR_VER                 6
+#define UTERROR_MALLOC              7
+#define UTERROR_ERASH_FLASH         8
+#define UTERROR_READ_FLASH          9
+#define UTERROR_WRITE_FLASH         10
+#define UTERROR_FW_SIZE             11
+#define UTERROR_BTL_SIZE            12
+
+/* error code use by ourself */
+#define UTERROR_SOCKET_INIT         -1
+#define UTERROR_CONNECT             -2
 
 
 #endif /* _UPDATE_THREAD_H_ */
--- Updater2/src/UpdateThread.cpp
+++ Updater2/src/UpdateThread.cpp
@@ -5,6 +5,7 @@
 #include <wx/thread.h>
 #include "UpdateThread.h"
 #include "WidgetId.h"
+#include "UpdaterApp.h"
 
 // ------------------------------------------------------------------------
 // Resources
@@ -13,50 +14,118 @@
 // ------------------------------------------------------------------------
 // Declaration
 // ------------------------------------------------------------------------
+#define RECVBUFSIZE                     128
+#define CONNECTION_TIMEOUT              5
+
 
 // ------------------------------------------------------------------------
 // Implementation
 // ------------------------------------------------------------------------
 UpdateThread::UpdateThread(wxEvtHandler *handler, const wxString &remote,
                            const int row)
-    : wxThread(wxTHREAD_DETACHED)
+    : wxThread(wxTHREAD_DETACHED),
+    _pHandler(handler),
+    _targetIpAddress(remote),
+    _row(row)
 {
-    _pHandler = handler;
-    _targetIpAddress = remote;
-    _row = row;
+    wxVector<NetAdapter> &netAdapter = wxGetApp().m_Adapters;
+    wxIPV4address local;
+
+    if (!netAdapter.empty())
+    {
+        _localIpAddress = netAdapter.at(0).GetIp();
+        local.Hostname(_localIpAddress);
+        _tcp = new wxSocketClient(wxSOCKET_BLOCK);
+    }
+    else
+    {
+        _localIpAddress = wxEmptyString;
+        _tcp = NULL;
+    }
+
+    _recvBuf = new unsigned char[RECVBUFSIZE];
 }
 
 UpdateThread::~UpdateThread()
 {
-
+    delete _tcp;
+    delete [] _recvBuf;
 }
 
 wxThread::ExitCode UpdateThread::Entry()
 {
     wxThreadEvent event(wxEVT_COMMAND_THREAD, myID_UPDATE_THREAD);
     UpdateThreadMessage msg;
+    wxIPV4address remote, local;
+    int error_code = 0;
+    unsigned int txSize = 0;
+    unsigned char *txBuf = NULL;
     int loop = 5;
+
+    unsigned char connectMessage[] =
+    {
+        0x53, 0x43, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+    };
     
     wxLogMessage(wxT("TCP udpate thread connect to %s"), _targetIpAddress);
 
-    wxSleep(5);
-
-    while (loop--)
+    if (_tcp)
     {
-        msg.type = UPDATE_THREAD_PROGRESS;
-        msg.progress = 100 - loop * 20;
-        msg.targetIpAddress = _targetIpAddress;
-        msg.row = _row;
-        event.SetPayload(msg);
-        wxQueueEvent(_pHandler, event.Clone());
+        remote.Hostname(_targetIpAddress);
+        remote.Service(40000);
+        local.Hostname(_localIpAddress);
 
-        wxSleep(2);
+        wxLogVerbose(wxT("Connect to %s:40000..."), _targetIpAddress);
+
+        if (_tcp->Connect(remote, local, false) || _tcp->WaitOnConnect(CONNECTION_TIMEOUT))
+        {
+            _tcp->Write(&connectMessage[0], sizeof(connectMessage));
+            _tcp->Read(_recvBuf, RECVBUFSIZE);
+            if ((_tcp->LastError() == wxSOCKET_NOERROR) 
+                && (_tcp->LastCount() == 16)
+                && (_recvBuf[0] == 's')
+                && (_recvBuf[1] == 'c')
+                && (_recvBuf[2] == 0)
+                && (_recvBuf[3] == 0))
+            {
+                if (_recvBuf[7] == 0) // big endian
+                    txSize = (_recvBuf[8] << 24) + (_recvBuf[9] << 16) + (_recvBuf[10] << 8) + _recvBuf[11];
+                else
+                    txSize = (_recvBuf[11] << 24) + (_recvBuf[10] << 16) + (_recvBuf[9] << 8) + _recvBuf[8];
+
+                txBuf = new unsigned char [txSize];
+            }
+
+            wxSleep(2);
+
+            while (loop--)
+            {
+                msg.type = UPDATE_THREAD_PROGRESS;
+                msg.progress = 100 - loop * 20;
+                msg.targetIpAddress = _targetIpAddress;
+                msg.row = _row;
+                event.SetPayload(msg);
+                wxQueueEvent(_pHandler, event.Clone());
+
+                wxSleep(2);
+            }
+        }
+        else
+            error_code = UTERROR_CONNECT;
     }
+    else
+        error_code = UTERROR_SOCKET_INIT;
 
     msg.type = UPDATE_THREAD_COMPLETED;
-    msg.error = 0;
+    msg.error = error_code;
     event.SetPayload(msg);
     wxQueueEvent(_pHandler, event.Clone());
 
-    return (ExitCode)0;
+    //
+    // clean up
+    //
+    delete [] txBuf;
+
+    return (ExitCode)error_code;
 }

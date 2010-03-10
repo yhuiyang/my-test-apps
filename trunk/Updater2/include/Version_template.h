c31c82256bfe0807f600c627040d8ddd006d3a9b
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2010-03-10 18:35:14 +0000

    Add version info for linux platform too.

--- trunk/Updater2/include/Version_template.h
+++ trunk/Updater2/include/Version_template.h
@@ -25,6 +25,13 @@
 #define VER_MAJOR               0
 #define VER_MINOR               5
 #define VER_RELEASE             0
+#define VER_BUILD
+#define STRINGIFY_HELPER(x)     #x
+#define STRINGIFY(x)            STRINGIFY_HELPER(x)
+#define VER_MAJOR_STRING        STRINGIFY(VER_MAJOR)
+#define VER_MINOR_STRING        STRINGIFY(VER_MINOR)
+#define VER_RELEASE_STRING      STRINGIFY(VER_RELEASE)
+#define VER_BUILD_STRING        STRINGIFY(VER_BUILD)
 
 #endif
 

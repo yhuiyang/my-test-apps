2df6acad150715ce7444e4f5b5ab4cfb456de744
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2010-03-10 03:56:57 +0000

    Let update thread determine image file type, and thread create determine the action (use thread constructor parameters)

--- trunk/Updater2/src/UpdateThread.cpp
+++ trunk/Updater2/src/UpdateThread.cpp
@@ -24,10 +24,11 @@
 // Implementation
 // ------------------------------------------------------------------------
 UpdateThread::UpdateThread(wxEvtHandler *handler, const wxString &codedString,
-                           const wxString &image)
+                           const wxString &image, const wxString &newMACAddress)
     : wxThread(wxTHREAD_DETACHED),
     _pHandler(handler),
-    _imageFilePath(image)
+    _imageFilePath(image),
+    _newMACAddress(newMACAddress)
 {
     wxVector<NetAdapter> &netAdapter = wxGetApp().m_Adapters;
     wxIPV4address local;
@@ -97,6 +98,10 @@ void UpdateThread::SendNotification(const UTMType type, const int data)
     wxQueueEvent(_pHandler, event.Clone());
 }
 
+#define ACTION_DOWNLOAD_FIRMWARE        0x01
+#define ACTION_DOWNLOAD_BOOTLOADER      0x05
+#define ACTION_ERASE_MANAGEMENT_BLOCK   0x06
+#define ACTION_UPDATE_MAC_ADDRESS       0x0F  // a random code number not used by the SOI updater and bootloader
 wxThread::ExitCode UpdateThread::Entry()
 {
     //
@@ -116,6 +121,7 @@ wxThread::ExitCode UpdateThread::Entry()
     //     01 - download firmware image to flash
     //     05 - download bootloader image to flash
     //     06 - reset reserved area
+    //     0F - update MAC address
     //    sequence number:
     //     ff ff (-1) - used in the last one payload
     //     00 00 (0)  - used in other payload
@@ -136,6 +142,7 @@ wxThread::ExitCode UpdateThread::Entry()
         0x53, 0x43, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0xFF,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
     };
+    bool updateFirmware = true, isImageFileFirstBlock = true;
 
     wxLogMessage(wxT("TCP udpate thread connect to %s"), _targetIpAddress);
 
@@ -166,117 +173,148 @@ wxThread::ExitCode UpdateThread::Entry()
                     txSize = (_recvBuf[11] << 24) + (_recvBuf[10] << 16) + (_recvBuf[9] << 8) + _recvBuf[8];
                 txBuf = new unsigned char [txSize];
 
-                /* start to load image file content and transmit it */
-                wxFFileInputStream imageStream(_imageFilePath);
-                if (imageStream.IsOk())
+                /*
+                   check what action should we do?
+                   if _imageFilePath is not empty, then do download firmware or bootloader depended on image size/content.
+                   else if _newMACAddress is not empty, then do update MAC address.
+                   else (both _imageFilePath and _newMACAddress are empty) do erase management block
+                 */
+                if (!_imageFilePath.empty()) // download firmware or bootloader
                 {
-                    long streamLength = imageStream.GetLength();
-                    long transmitCount, transmitDataSize, q, r;
-                    unsigned int checkSum = 0;
-                    int transmitCurrentProgress = 0, transmitStep;
-
-                    q = streamLength / (txSize - 16);
-                    r = streamLength % (txSize - 16);
-                    if (r)
-                        transmitCount = q + 1;
-                    else
+                    /* start to load image file content and transmit it */
+                    wxFFileInputStream imageStream(_imageFilePath);
+                    if (imageStream.IsOk())
                     {
-                        transmitCount = q;
-                        r = txSize - 16;
-                    }
+                        long streamLength = imageStream.GetLength();
+                        long transmitCount, transmitDataSize, q, r;
+                        unsigned int checkSum = 0;
+                        int transmitCurrentProgress = 0, transmitStep;
 
-                    transmitStep = 100 / transmitCount;
-
-                    // to here: transmitCount is the number of transmit, and r is last transmition size.
-                    transmitDataSize = txSize - 16;
-
-                    while (transmitCount--)
-                    {
-                        size_t readIndex, leftForRead, lastRead, sumIndex;
-
-                        if (transmitCount == 0)
-                            transmitDataSize = r;
-
-                        readIndex = 16;
-                        leftForRead = transmitDataSize;
-
-                        /* read required data length from stream */
-                        do
-                        {
-                            imageStream.Read(&txBuf[readIndex], leftForRead);
-                            lastRead = imageStream.LastRead();
-                            readIndex += lastRead;
-                            leftForRead -= lastRead;
-                        } while (leftForRead != 0);
-
-                        /* update the calculated checksum */
-                        for (sumIndex = 16; sumIndex < readIndex; sumIndex++)
-                            checkSum += txBuf[sumIndex];
-
-                        /* prepare the load header */
-                        txBuf[0] = 'T';
-                        txBuf[1] = 'B';
-                        txBuf[2] = 0xff;
-                        txBuf[3] = 0x01; // firmware
-                        txBuf[4] = 0;
-                        txBuf[5] = 0;
-                        if (transmitCount == 0)
-                        {
-                            txBuf[6] = txBuf[7] = 0xff;
-                            txBuf[12] = (checkSum >> 24) & 0xFF;
-                            txBuf[13] = (checkSum >> 16) & 0xFF;
-                            txBuf[14] = (checkSum >> 8) & 0xFF;
-                            txBuf[15] = checkSum & 0xFF;
-                        }
+                        q = streamLength / (txSize - 16);
+                        r = streamLength % (txSize - 16);
+                        if (r)
+                            transmitCount = q + 1;
                         else
                         {
-                            txBuf[6] = txBuf[7] = 0;
-                            txBuf[12] = txBuf[13] = txBuf[14] = txBuf[15] = 0;
-                        }
-                        txBuf[8] = (transmitDataSize >> 24) & 0xFF;
-                        txBuf[9] = (transmitDataSize >> 16) & 0xFF;
-                        txBuf[10] = (transmitDataSize >> 8) & 0xFF;
-                        txBuf[11] = transmitDataSize & 0xFF;
-
-                        /* transmit data */
-                        _tcp->Write(&txBuf[0], transmitDataSize + 16);
-                        if (_tcp->LastError() != wxSOCKET_NOERROR)
-                        {
-                            error_code = UTERROR_SOCKET_WRITE;
-                            break;
+                            transmitCount = q;
+                            r = txSize - 16;
                         }
 
-                        /* wait for response and verify it */
-                        _tcp->Read(_recvBuf, RECVBUFSIZE);
-                        if ((_tcp->LastError() == wxSOCKET_NOERROR)
-                            && (_tcp->LastCount() == 16))
-                        {
-                            wxLogVerbose(wxT("Response: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X [%08X]"),
-                                _recvBuf[0], _recvBuf[1], _recvBuf[2], _recvBuf[3], _recvBuf[4], _recvBuf[5], _recvBuf[6], _recvBuf[7],
-                                _recvBuf[8], _recvBuf[9], _recvBuf[10], _recvBuf[11], _recvBuf[12], _recvBuf[13], _recvBuf[14], _recvBuf[15], checkSum);
+                        transmitStep = 100 / transmitCount;
 
-                            if ((_recvBuf[4] != 0) || (_recvBuf[5] != 0))
+                        // to here: transmitCount is the number of transmit, and r is last transmition size.
+                        transmitDataSize = txSize - 16;
+
+                        while (transmitCount--)
+                        {
+                            size_t readIndex, leftForRead, lastRead, sumIndex;
+
+                            if (transmitCount == 0)
+                                transmitDataSize = r;
+
+                            readIndex = 16;
+                            leftForRead = transmitDataSize;
+
+                            /* read required data length from stream */
+                            do
                             {
-                                error_code = (_recvBuf[4] << 8) + _recvBuf[5];
+                                imageStream.Read(&txBuf[readIndex], leftForRead);
+                                lastRead = imageStream.LastRead();
+                                readIndex += lastRead;
+                                leftForRead -= lastRead;
+                            } while (leftForRead != 0);
+
+                            /* check image content to verify brec correctness */
+                            if (isImageFileFirstBlock)
+                            {
+                                isImageFileFirstBlock = false;
+                                if ((txBuf[16] != 'S') || (txBuf[17] != 'x'))
+                                {
+                                    error_code = UTERROR_BREC_SYNTAX;
+                                    break;
+                                }
+
+                                if (transmitCount == 0) // assume bootloader's size is never over one load payload size, and firmware does.
+                                    updateFirmware = false;
+                            }
+
+                            /* update the calculated checksum */
+                            for (sumIndex = 16; sumIndex < readIndex; sumIndex++)
+                                checkSum += txBuf[sumIndex];
+
+                            /* prepare the load header */
+                            txBuf[0] = 'T';
+                            txBuf[1] = 'B';
+                            txBuf[2] = 0xff;
+                            txBuf[3] = updateFirmware ? ACTION_DOWNLOAD_FIRMWARE : ACTION_DOWNLOAD_BOOTLOADER;
+                            txBuf[4] = 0;
+                            txBuf[5] = 0;
+                            if (transmitCount == 0)
+                            {
+                                txBuf[6] = txBuf[7] = 0xff;
+                                txBuf[12] = (checkSum >> 24) & 0xFF;
+                                txBuf[13] = (checkSum >> 16) & 0xFF;
+                                txBuf[14] = (checkSum >> 8) & 0xFF;
+                                txBuf[15] = checkSum & 0xFF;
+                            }
+                            else
+                            {
+                                txBuf[6] = txBuf[7] = 0;
+                                txBuf[12] = txBuf[13] = txBuf[14] = txBuf[15] = 0;
+                            }
+                            txBuf[8] = (transmitDataSize >> 24) & 0xFF;
+                            txBuf[9] = (transmitDataSize >> 16) & 0xFF;
+                            txBuf[10] = (transmitDataSize >> 8) & 0xFF;
+                            txBuf[11] = transmitDataSize & 0xFF;
+
+                            /* transmit data */
+                            _tcp->Write(&txBuf[0], transmitDataSize + 16);
+                            if (_tcp->LastError() != wxSOCKET_NOERROR)
+                            {
+                                error_code = UTERROR_SOCKET_WRITE;
                                 break;
                             }
-                        }
-                        else
-                        {
-                            error_code = UTERROR_SOCKET_READ;
-                            break;
-                        }
 
-                        /* notify progress */
-                        if (transmitCount == 0)
-                            transmitCurrentProgress = 100;
-                        else
-                            transmitCurrentProgress += transmitStep;
-                        SendNotification(UPDATE_THREAD_PROGRESS, transmitCurrentProgress);
+                            /* wait for response and verify it */
+                            _tcp->Read(_recvBuf, RECVBUFSIZE);
+                            if ((_tcp->LastError() == wxSOCKET_NOERROR)
+                                && (_tcp->LastCount() == 16))
+                            {
+                                wxLogVerbose(wxT("Response: %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X [%08X]"),
+                                    _recvBuf[0], _recvBuf[1], _recvBuf[2], _recvBuf[3], _recvBuf[4], _recvBuf[5], _recvBuf[6], _recvBuf[7],
+                                    _recvBuf[8], _recvBuf[9], _recvBuf[10], _recvBuf[11], _recvBuf[12], _recvBuf[13], _recvBuf[14], _recvBuf[15], checkSum);
+
+                                if ((_recvBuf[4] != 0) || (_recvBuf[5] != 0))
+                                {
+                                    error_code = (_recvBuf[4] << 8) + _recvBuf[5];
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                error_code = UTERROR_SOCKET_READ;
+                                break;
+                            }
+
+                            /* notify progress */
+                            if (transmitCount == 0)
+                                transmitCurrentProgress = 100;
+                            else
+                                transmitCurrentProgress += transmitStep;
+                            SendNotification(UPDATE_THREAD_PROGRESS, transmitCurrentProgress);
+                        }
                     }
+                    else
+                        error_code = UTERROR_FILE_STREAM;
                 }
-                else
-                    error_code = UTERROR_FILE_STREAM;
+                else if (!_newMACAddress.empty()) // update MAC address
+                {
+
+                }
+                else // erase management block
+                {
+
+                }
             }
             else
                 error_code = UTERROR_CONNECT;

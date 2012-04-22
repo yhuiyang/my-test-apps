1367a7f8dae03b52befc77484e105eb7c54ea1ef
YH Yang <yhuiyang@gmail.com>
2012-04-22 01:52:42 +0000

    Add basic activity lifecycle callback methods.

--- AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/CMActivity.java
+++ AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/CMActivity.java
@@ -2,12 +2,74 @@ package com.yhlab.commitmonitor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class CMActivity extends Activity {
+
+    static final String TAG = "CMActivity";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        
+        Log.d(TAG, "onRestoreInstanceState");
     }
 }

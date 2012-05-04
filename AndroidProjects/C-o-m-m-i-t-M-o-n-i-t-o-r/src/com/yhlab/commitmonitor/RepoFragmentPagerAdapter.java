d0f559f1df80fe195035aa77a07dd0fc10e970f5
YH Yang <yhuiyang@gmail.com>
2012-05-04 13:31:35 +0000

    Use viewpager for sw600dp.

---
+++ AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/RepoFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.yhlab.commitmonitor;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
+public class RepoFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    final static String TAG = "RepoFragmentPagerAdapter";
+    
+    public RepoFragmentPagerAdapter(FragmentManager fragMgr) {
+        super(fragMgr);
+    }
+    
+    @Override
+    public Fragment getItem(int arg0) {
+        Log.d(TAG, "getItem" + arg0);
+        
+        switch (arg0) {
+        case 0:
+            return new RepoConfigFragment();
+        case 1:
+            return new RepoDetailFragment();
+        default:
+            Log.e(TAG, "Don't have so many fragment.");
+            return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+}

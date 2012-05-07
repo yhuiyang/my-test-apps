05bbc5e70a8edae1ff0b931cbf6cd4c1ad109e79
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-05-07 03:59:02 +0000

    Add AdMob ad.

--- trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/CMActivity.java
+++ trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/CMActivity.java
@@ -4,6 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.widget.LinearLayout;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
 
 public class CMActivity extends FragmentActivity {
 
@@ -12,6 +17,14 @@ public class CMActivity extends FragmentActivity {
     /* for viewpager management used in tablet */
     private RepoFragmentPagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
+
+    /* ad */
+    private AdSize adSize;
+    private AdView adView;
+    private LinearLayout adLayout;
+    private boolean adAvailable = true;
+    private int minHeight;
+    private int minWidth;
 
     /** Called when the activity is first created. */
     @Override
@@ -24,6 +37,41 @@ public class CMActivity extends FragmentActivity {
             mPagerAdapter = new RepoFragmentPagerAdapter(
                     getSupportFragmentManager());
             mViewPager.setAdapter(mPagerAdapter);
+        }
+
+        /* Determinate ad size and then load ad */
+        if (null != (adLayout = (LinearLayout) findViewById(R.id.adLayoutIABLeaderBoard))) {
+            Log.d(TAG,
+                    "land - " + adLayout.getWidth() + ", "
+                            + adLayout.getHeight());
+            adSize = AdSize.IAB_LEADERBOARD;
+            minHeight = 90;
+            minWidth = 728;
+        } else if (null != (adLayout = (LinearLayout) findViewById(R.id.adLayoutIABBanner))) {
+            Log.d(TAG,
+                    "port - " + adLayout.getWidth() + ", "
+                            + adLayout.getHeight());
+            adSize = AdSize.IAB_BANNER;
+            minHeight = 60;
+            minWidth = 468;
+        } else if (null != (adLayout = (LinearLayout) findViewById(R.id.adLayoutBanner))) {
+            adSize = AdSize.BANNER;
+            minHeight = 50;
+            minWidth = 320;
+        } else {
+            adAvailable = false;
+        }
+
+        if (adAvailable) {
+            adView = new AdView(this, adSize, "a14fa492f7183ee");
+            adView.setMinimumHeight(minHeight);
+            adView.setMinimumWidth(minWidth);
+            adLayout.addView(adView, 0);
+            AdRequest adRequest = new AdRequest();
+            // adRequest addTestDevice();
+            adRequest.addTestDevice("310E735B8E260B98F50AD7FBAEB83D74");
+            adRequest.addTestDevice("7370AF93550EB18283A7EA7D78D020B7");
+            adView.loadAd(adRequest);
         }
 
         Log.d(TAG, "onCreate");
@@ -66,6 +114,13 @@ public class CMActivity extends FragmentActivity {
 
     @Override
     protected void onDestroy() {
+
+        /* remove ad from layout first or crash */
+        if (adAvailable) {
+            adLayout.removeView(adView);
+            adView.destroy();
+        }
+
         super.onDestroy();
 
         Log.d(TAG, "onDestroy");

31910cb6c401780d1355c384b018ea9416502306
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-05-31 03:01:07 +0000

    update fragment transaction.

--- trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/FirstUseFragment.java
+++ trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/FirstUseFragment.java
@@ -1,14 +1,30 @@
 package com.yhlab.commitmonitor;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
-public class FirstUseFragment extends Fragment {
+public class FirstUseFragment extends Fragment implements OnClickListener {
     private static final String TAG = "FirstUseFragment";
+
+    INextButtonClickListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            mListener = (INextButtonClickListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement INextButtonClickListener interface.");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,5 +36,37 @@ public class FirstUseFragment extends Fragment {
 
         Log.v(TAG, "<<< onCreateView");
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        Log.v(TAG, ">>> onViewCreated");
+
+        super.onViewCreated(view, savedInstanceState);
+
+        View btn = view.findViewById(R.id.btn_next_add_repo);
+        btn.setOnClickListener(this);
+
+        Log.v(TAG, "<<< onViewCreated");
+    }
+
+    /**
+     * The interface(s) that the container activity must implement
+     */
+    public interface INextButtonClickListener {
+        public void onNextButtonClick();
+    }
+
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+        case R.id.btn_next_add_repo:
+            mListener.onNextButtonClick();
+            break;
+        default:
+            Log.w(TAG, "Unhandled click event: id = " + id);
+            break;
+        }
     }
 }

c3d418e03247a984adf154fa3884070ca24faad7
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-06-02 02:42:49 +0000

    Add menu items in repo config fragment, and remove unused fragment callback functions.

--- trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/RepoConfigFragment.java
+++ trunk/AndroidProjects/C-o-m-m-i-t-M-o-n-i-t-o-r/src/com/yhlab/commitmonitor/RepoConfigFragment.java
@@ -1,10 +1,12 @@
 package com.yhlab.commitmonitor;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,21 +15,12 @@ public class RepoConfigFragment extends Fragment {
     static final String TAG = "RepoConfigFragment";
 
     @Override
-    public void onAttach(Activity activity) {
-
-        Log.v(TAG, ">>> onAttach");
-
-        super.onAttach(activity);
-
-        Log.v(TAG, "<<< onAttach");
-    }
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
 
         Log.v(TAG, ">>> onCreate");
 
         super.onCreate(savedInstanceState);
+        this.setHasOptionsMenu(true);
 
         Log.v(TAG, "<<< onCreate");
     }
@@ -48,82 +41,17 @@ public class RepoConfigFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-
-        Log.v(TAG, ">>> onActivityCreated");
-
-        super.onActivityCreated(savedInstanceState);
-
-        Log.v(TAG, "<<< onActivityCreated");
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.repoconfig_optmenu, menu);
     }
 
     @Override
-    public void onStart() {
-
-        Log.v(TAG, ">>> onStart");
-
-        super.onStart();
-
-        Log.v(TAG, "<<< onStart");
-    }
-
-    @Override
-    public void onResume() {
-
-        Log.v(TAG, ">>> onResume");
-
-        super.onResume();
-
-        Log.v(TAG, "<<< onResume");
-    }
-
-    @Override
-    public void onPause() {
-
-        Log.v(TAG, ">>> onPause");
-
-        super.onPause();
-
-        Log.v(TAG, "<<< onPause");
-    }
-
-    @Override
-    public void onStop() {
-
-        Log.v(TAG, ">>> onStop");
-
-        super.onStop();
-
-        Log.v(TAG, "<<< onStop");
-    }
-
-    @Override
-    public void onDestroyView() {
-
-        Log.v(TAG, ">>> onDestroyView");
-
-        super.onDestroyView();
-
-        Log.v(TAG, "<<< onDestroyView");
-    }
-
-    @Override
-    public void onDestroy() {
-
-        Log.v(TAG, ">>> onDestroy");
-
-        super.onDestroy();
-
-        Log.v(TAG, "<<< onDestroy");
-    }
-
-    @Override
-    public void onDetach() {
-
-        Log.v(TAG, ">>> onDetach");
-
-        super.onDetach();
-
-        Log.v(TAG, "<<< onDetach");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.optitem_add_repo_sample:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 }

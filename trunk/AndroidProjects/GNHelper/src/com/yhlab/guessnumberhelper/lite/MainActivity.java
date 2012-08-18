40f7314933eeb3dbdd311fa681b1a77e9f62a192
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-08-18 01:05:06 +0000

    Add implementation of onGuess.

--- trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/MainActivity.java
+++ trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/MainActivity.java
@@ -1,10 +1,12 @@
 package com.yhlab.guessnumberhelper.lite;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements
+        NumberFragment.IGuessedListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,13 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    public void onGuess(int number, int result) {
+        FragmentManager fm = getSupportFragmentManager();
+        HistoryFragment hf = (HistoryFragment) fm
+                .findFragmentById(R.id.history_fragment);
+        hf.addHistory(number, result);
     }
 }

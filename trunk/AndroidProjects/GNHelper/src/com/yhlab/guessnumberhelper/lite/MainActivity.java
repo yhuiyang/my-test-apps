caf9fd4cd3a411479d43584160a195761d120071
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-08-20 13:52:26 +0000

    Register/unregister OnSharedPreferenceChangeListener in main activity.

--- trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/MainActivity.java
+++ trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/MainActivity.java
@@ -1,24 +1,49 @@
 package com.yhlab.guessnumberhelper.lite;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity implements
-        NumberFragment.IGuessedListener {
+        NumberFragment.IGuessedListener, OnSharedPreferenceChangeListener {
+
+    private SharedPreferences mSharedPrefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         /* init default values */
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-        
+
         setContentView(R.layout.activity_main);
+
+        /*
+         * register sharedPreferences changed event. If event happened at the
+         * time between event unregister and register, the event handler will
+         * not be fired. In our case, because the sharedPreferences is changed
+         * within another activity which was invoked by this main activity,
+         * don't register and unregister event in the onResume and onPause
+         * callback method.
+         */
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        mSharedPrefs.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        /* unregister sharedPreferences changed event */
+        mSharedPrefs.unregisterOnSharedPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -26,10 +51,10 @@ public class MainActivity extends FragmentActivity implements
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        
+
         switch (item.getItemId()) {
         case R.id.menu_settings:
             this.startActivity(new Intent(this, SettingsActivity.class));
@@ -47,5 +72,17 @@ public class MainActivity extends FragmentActivity implements
         HistoryFragment hf = (HistoryFragment) fm
                 .findFragmentById(R.id.history_fragment);
         hf.addHistory(number, result);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sp, String k) {
+
+        if (k.equals(SettingsActivity.KEY_DIGIT_COUNT)) {
+            String s1 = sp.getString(k, null);
+            Log.d("MainActivity", "change digit count to " + s1);
+        } else if (k.equals(SettingsActivity.KEY_GUESS_METHOD)) {
+            String s2 = sp.getString(k, null);
+            Log.d("MainActivity", "change guess method to " + s2);
+        }
     }
 }

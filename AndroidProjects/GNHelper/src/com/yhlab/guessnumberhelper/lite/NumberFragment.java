2cdfd0e86ee8ea48eac458e2dca7eca840c15f15
YH Yang <yhuiyang@gmail.com>
2012-08-20 01:03:30 +0000

    Add GuessedNumber, GuessedResult widgets by parameter in SharedPreference.

--- AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/NumberFragment.java
+++ AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/NumberFragment.java
@@ -1,13 +1,16 @@
 package com.yhlab.guessnumberhelper.lite;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableRow;
 import com.yhlab.component.guessnumber.GuessedNumber;
 import com.yhlab.component.guessnumber.GuessedResult;
 
@@ -37,15 +40,26 @@ public class NumberFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        int digitCount = Integer.parseInt(sp.getString(SettingsActivity.KEY_DIGIT_COUNT, "4"));
        
+        /* add dynamic widgets GuessedNumber */
+        final GuessedNumber gn = new GuessedNumber(getActivity(), digitCount, "0123456789?");
+        TableRow tr = (TableRow)getView().findViewById(R.id.row_guess);
+        tr.addView(gn);
+        
+        /* add dynamic widgets GuessedResult */
+        final GuessedResult gr = new GuessedResult(getActivity(), digitCount);
+        tr = (TableRow)getView().findViewById(R.id.row_result);
+        tr.addView(gr, 1);
+        
         Button add = (Button) (getView().findViewById(R.id.btn_result_add));
         add.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                GuessedNumber n = (GuessedNumber) (getView().findViewById(R.id.guessed_number));
-                GuessedResult r = (GuessedResult) (getView().findViewById(R.id.guessed_result));
-                mGuessedListener.onGuess(n.getNumber(), r.getResult());
+                mGuessedListener.onGuess(gn.getNumber(), gr.getResult());
             }
 
         });

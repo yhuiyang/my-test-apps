893d26a56b3f811bbb362920b3a5781daca0e330
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-08-24 06:44:55 +0000

    No code change, just reformat(ctrl+shift+f).

--- trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/guess/IntArray.java
+++ trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/guess/IntArray.java
@@ -1,9 +1,9 @@
 package com.yhlab.guessnumberhelper.guess;
 
 /**
- * This class is an extendable array implementation for the primitive 
- * type <code>int</code>. 
- *
+ * This class is an extendable array implementation for the primitive type
+ * <code>int</code>.
+ * 
  * @author Cheng-Ru Lin ( owenlin.twn@gmail.com )
  */
 public class IntArray {
@@ -11,74 +11,79 @@ public class IntArray {
     private int[] content;
     private int size = 0;
 
-    /** 
+    /**
      * Constructs an instance of <code>IntArray</code> with totally
-     * <code>size</code> elements. Please note that, the size of the
-     * created array is still 0.
+     * <code>size</code> elements. Please note that, the size of the created
+     * array is still 0.
      * 
-     * @param capacity the initial capacity
+     * @param capacity
+     *            the initial capacity
      */
-    public IntArray( int capacity ) {
-        if ( capacity < 0 )
+    public IntArray(int capacity) {
+        if (capacity < 0)
             throw new IllegalArgumentException();
-        content = new int[ capacity ];      
+        content = new int[capacity];
     }
-    
-    /** 
+
+    /**
      * Constructs an empty instance of <code>IntArray</code>.
      */
     public IntArray() {
-        this( DEFAULT_INIT_SIZE );
+        this(DEFAULT_INIT_SIZE);
     }
-    
+
     /**
-     * Empties the elements in this array. After clearing, the size 
-     * of this <code>IntArray</code> will be "0".
+     * Empties the elements in this array. After clearing, the size of this
+     * <code>IntArray</code> will be "0".
      */
     public void clear() {
         this.size = 0;
     }
-    
+
     /**
-     * Appends one elements to the tail of the <code>IntArray</code>.
-     * After this operation, the size of the <code>IntArray</code> 
-     * will be increased by one.
+     * Appends one elements to the tail of the <code>IntArray</code>. After this
+     * operation, the size of the <code>IntArray</code> will be increased by
+     * one.
      * 
-     * @param value the value to be appended
+     * @param value
+     *            the value to be appended
      */
-    public void add( int value ) {
-        if ( size == content.length ) {
-            int buffer[] = new int[ Math.max( 4, size + size ) ];
-            System.arraycopy( content, 0, buffer, 0, size );
+    public void add(int value) {
+        if (size == content.length) {
+            int buffer[] = new int[Math.max(4, size + size)];
+            System.arraycopy(content, 0, buffer, 0, size);
             this.content = buffer;
         }
-        content[ size++ ] = value;
+        content[size++] = value;
     }
-    
+
     /**
      * Gets the value of the element of the given index.
      * 
-     * @param index the index of the returned value
+     * @param index
+     *            the index of the returned value
      * @return the value of the required element
      */
-    public int get( int index ) {
-        if ( index < 0 || index >= size )
-            throw new ArrayIndexOutOfBoundsException( index );
-        return content[ index ];
+    public int get(int index) {
+        if (index < 0 || index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        return content[index];
     }
-    
+
     /**
      * Sets the value of the element of the given index.
      * 
-     * @param index the index of the element to be set
-     * @param value the value to be set
+     * @param index
+     *            the index of the element to be set
+     * @param value
+     *            the value to be set
      */
-    public void set( int index, int value ) {
-        if ( index < 0 || index >= size )
-            throw new ArrayIndexOutOfBoundsException( index );
-        content[ index ] = value;
+    public void set(int index, int value) {
+        if (index < 0 || index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        content[index] = value;
     }
-    
+
     /**
      * Gets the size of this <code>IntArray</code>.
      * 
@@ -87,36 +92,39 @@ public class IntArray {
     public int size() {
         return size;
     }
-    
+
     /**
-     * Sets the size of this <code>IntArray</code>. If the new size
-     * is greater than the current size, the new added elements will 
-     * be all initialized as "0". If the new size is smaller than 
-     * the current size, the tail elements will be truncated. 
+     * Sets the size of this <code>IntArray</code>. If the new size is greater
+     * than the current size, the new added elements will be all initialized as
+     * "0". If the new size is smaller than the current size, the tail elements
+     * will be truncated.
      * 
-     * @param size the new size of this <code>IntArray</code>
-     * @throws IllegalArgumentException if <code>size</code> &lt; 0
+     * @param size
+     *            the new size of this <code>IntArray</code>
+     * @throws IllegalArgumentException
+     *             if <code>size</code> &lt; 0
      */
-    public void setSize( int size ) {
-        if ( size < 0 ) throw new IllegalArgumentException();
-        if ( size == this.size ) return;
-        int buf[] = new int[ size ];
-        System.arraycopy( this.content, 
-                0, buf, 0, Math.min( this.size, size ) );
+    public void setSize(int size) {
+        if (size < 0)
+            throw new IllegalArgumentException();
+        if (size == this.size)
+            return;
+        int buf[] = new int[size];
+        System.arraycopy(this.content,
+                0, buf, 0, Math.min(this.size, size));
         this.content = buf;
     }
-    
+
     /**
-     * Converts the content into a traditional <code>int</code> 
-     * array.
+     * Converts the content into a traditional <code>int</code> array.
      * 
-     * @return an <code>int</code> array of the elements in this 
-     *      <code>IntArray</code> 
+     * @return an <code>int</code> array of the elements in this
+     *         <code>IntArray</code>
      */
     public int[] toArray() {
-        int ret[] = new int[ size ];
-        System.arraycopy( content, 0, ret, 0, size );
+        int ret[] = new int[size];
+        System.arraycopy(content, 0, ret, 0, size);
         return ret;
     }
-    
+
 }

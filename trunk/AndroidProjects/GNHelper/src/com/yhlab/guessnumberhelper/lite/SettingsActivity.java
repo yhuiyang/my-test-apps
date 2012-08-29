9be90000935b4eda3011f3c0b9ed0f5460a274f3
yhuiyang <yhuiyang@3ac4a3f8-9f18-11dd-9dc9-65bc890fb60a>
2012-08-29 05:23:31 +0000

    Also use actionbarsherlock in the setting activity.

--- trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/SettingsActivity.java
+++ trunk/AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/SettingsActivity.java
@@ -1,9 +1,9 @@
 package com.yhlab.guessnumberhelper.lite;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends SherlockPreferenceActivity {
     
     public static final String KEY_DIGIT_COUNT = "prefs_digitCount";
     public static final String KEY_GUESS_METHOD = "prefs_guessMethod";

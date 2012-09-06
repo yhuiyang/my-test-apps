0def6c9b0ab3c88130639e471cdee43c2741535c
YH Yang <yhuiyang@gmail.com>
2012-09-06 07:58:54 +0000

    Update the textviews to display guess history.

--- AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/HistoryFragment.java
+++ AndroidProjects/GNHelper/src/com/yhlab/guessnumberhelper/lite/HistoryFragment.java
@@ -9,7 +9,11 @@ import com.actionbarsherlock.app.SherlockFragment;
 
 public class HistoryFragment extends SherlockFragment {
 
+    @SuppressWarnings("unused")
+    private static final String TAG = "HistoryFragment";
     private int guessCount = 0;
+    private TextView tv1;
+    private TextView tv2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,17 +24,33 @@ public class HistoryFragment extends SherlockFragment {
         return v;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tv1 = (TextView) getView().findViewById(R.id.tv_guessed_history);
+        tv2 = (TextView) getView().findViewById(R.id.tv_guessed_history_2);
+    }
+
     public void addHistory(int number, int result) {
-        TextView tv = (TextView) getView()
-                .findViewById(R.id.tv_guessed_history);
-        tv.append(String.format("[%d] %04X %XA%XB\n", ++guessCount, number,
+
+        TextView tv;
+
+        if ((guessCount++ % 2) == 1 && tv2 != null)
+            tv = tv2;
+        else
+            tv = tv1;
+
+        tv.append(String.format("[%d] %04X %XA%XB\n", guessCount, number,
                 result >> 4, result & 0xF));
     }
 
     public void clearHistory() {
-        TextView tv = (TextView) getView()
-                .findViewById(R.id.tv_guessed_history);
-        tv.setText(null);
+
+        tv1.setText(null);
+        if (tv2 != null)
+            tv2.setText(null);
+
         guessCount = 0;
     }
 }
